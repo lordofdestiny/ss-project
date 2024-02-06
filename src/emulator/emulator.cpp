cad9b781f4fa892d818223c833356fcd106c4e2c
lod <djumicbranislav@outlook.com>
2024-02-06 20:27:07 +0100

    Simplified emulator

--- src/emulator/emulator.cpp
+++ src/emulator/emulator.cpp
@@ -22,46 +22,53 @@ namespace m_emu {
         OTHER
     };
 
+    void handle_interrupt(context_t &context, const uint32_t cause) {
+        // If interrupts are masked
+        if (context.cause & 0b100) return;
+
+        // push status
+        context.sp -= 4;
+        context.write_to(context.sp, context.status);
+        // push pc
+        context.sp -= 4;
+        context.write_to(context.sp, context.pc);
+
+        context.cause = cause;
+        context.status |= 0x100;
+        context.pc = context.handler;
+    }
+
     void emulate(context_t &context) {
         using namespace std::string_literals;
         bool done = false;
         while (!done) {
-            const auto instr = context.read_instr();
-            switch (static_cast<instr_code>(instr.code)) {
+            const auto [code, mode, reg_a, reg_b, reg_c, disp] = context.read_instr();
+            switch (static_cast<instr_code>(code)) {
             case HALT: {
                 done = true;
                 break;
             }
             case INT: {
-                // push status
-                context.sp -= 4;
-                context.write_to(context.sp, context.status);
-                // push pc
-                context.sp -= 4;
-                context.write_to(context.sp, context.pc);
-
-                context.cause = 4;
-                context.status = context.status & ~0x1;
-                context.pc = context.handler;
+                handle_interrupt(context, 4);
                 break;
             }
             case CALL: {
                 using call_mode = common::instruction_t::call_mode;
-                switch (static_cast<call_mode>(instr.mode)) {
+                switch (static_cast<call_mode>(mode)) {
                 case call_mode::REG_IND_DISP:
                     context.sp -= 4;
                     context.write_to(context.sp, context.pc);
-                    context.pc = context.gpr[instr.reg1] + context.gpr[instr.reg2] + instr.disp;
+                    context.pc = context.gpr[reg_a] + context.gpr[reg_b] + disp;
                     break;
                 case call_mode::MEM_IND_DISP:
                     context.sp -= 4;
                     context.write_to(context.sp, context.pc);
                     context.pc = context.read_word(
-                        context.gpr[instr.reg1] + context.gpr[instr.reg2] + instr.disp);
+                        context.gpr[reg_a] + context.gpr[reg_b] + disp);
                     break;
                 default: {
                     std::stringstream ss;
-                    ss << "Invalid mode for call instruction: " << instr.mode;
+                    ss << "Invalid mode for call instruction: " << mode;
                     throw std::logic_error(ss.str());
                 }
                 }
@@ -69,75 +76,75 @@ namespace m_emu {
             }
             case JMP: {
                 using jump_mode = common::instruction_t::jump_mode;
-                switch (static_cast<jump_mode>(instr.mode)) {
+                switch (static_cast<jump_mode>(mode)) {
                 case jump_mode::REG_DIR_DISP:
-                    context.pc = context.gpr[instr.reg1] + instr.disp;
+                    context.pc = context.gpr[reg_a] + disp;
                     break;
                 case jump_mode::EQ_REG_DIR_DISP:
-                    if (context.gpr[instr.reg2] == context.gpr[instr.reg3]) {
-                        context.pc = context.gpr[instr.reg1] + instr.disp;
+                    if (context.gpr[reg_b] == context.gpr[reg_c]) {
+                        context.pc = context.gpr[reg_a] + disp;
                     }
                     break;
                 case jump_mode::NE_REG_DIR_DISP:
-                    if (context.gpr[instr.reg2] != context.gpr[instr.reg3]) {
-                        context.pc = context.gpr[instr.reg1] + instr.disp;
+                    if (context.gpr[reg_b] != context.gpr[reg_c]) {
+                        context.pc = context.gpr[reg_a] + disp;
                     }
                     break;
                 case jump_mode::GT_REG_DIR_DISP:
-                    if (static_cast<int32_t>(context.gpr[instr.reg2])
-                        > static_cast<int32_t>(context.gpr[instr.reg3])) {
-                        context.pc = context.gpr[instr.reg1] + instr.disp;
+                    if (static_cast<int32_t>(context.gpr[reg_b])
+                        > static_cast<int32_t>(context.gpr[reg_c])) {
+                        context.pc = context.gpr[reg_a] + disp;
                     }
                     break;
                 case jump_mode::REG_IND_DISP:
-                    context.pc = context.read_word(context.gpr[instr.reg1] + instr.disp);
+                    context.pc = context.read_word(context.gpr[reg_a] + disp);
                     break;
                 case jump_mode::EQ_REG_IND_DISP:
-                    if (context.gpr[instr.reg2] == context.gpr[instr.reg3]) {
-                        context.pc = context.read_word(context.gpr[instr.reg1] + instr.disp);
+                    if (context.gpr[reg_b] == context.gpr[reg_c]) {
+                        context.pc = context.read_word(context.gpr[reg_a] + disp);
                     }
                     break;
                 case jump_mode::NE_REG_IND_DISP:
-                    if (context.gpr[instr.reg2] != context.gpr[instr.reg3]) {
-                        context.pc = context.read_word(context.gpr[instr.reg1] + instr.disp);
+                    if (context.gpr[reg_b] != context.gpr[reg_c]) {
+                        context.pc = context.read_word(context.gpr[reg_a] + disp);
                     }
                     break;
                 case jump_mode::GT_REG_IND_DISP:
-                    if (static_cast<int32_t>(context.gpr[instr.reg2])
-                        > static_cast<int32_t>(context.gpr[instr.reg3])) {
-                        context.pc = context.read_word(context.gpr[instr.reg1] + instr.disp);
+                    if (static_cast<int32_t>(context.gpr[reg_b])
+                        > static_cast<int32_t>(context.gpr[reg_c])) {
+                        context.pc = context.read_word(context.gpr[reg_a] + disp);
                     }
                     break;
                 default: {
                     std::stringstream ss;
-                    ss << "Invalid mode for jump instruction: " << instr.mode;
+                    ss << "Invalid mode for jump instruction: " << mode;
                     throw std::logic_error(ss.str());
                 }
                 }
                 break;
             }
             case XCHG: {
-                std::swap(context.gpr[instr.reg2], context.gpr[instr.reg3]);
+                std::swap(context.gpr[reg_b], context.gpr[reg_c]);
                 break;
             }
             case MATH: {
                 using arithmetic_mode = common::instruction_t::arithmetic_mode;
-                switch (static_cast<arithmetic_mode>(instr.mode)) {
+                switch (static_cast<arithmetic_mode>(mode)) {
                 case arithmetic_mode::ADD:
-                    context.gpr[instr.reg1] = context.gpr[instr.reg2] + context.gpr[instr.reg3];
+                    context.gpr[reg_a] = context.gpr[reg_b] + context.gpr[reg_c];
                     break;
                 case arithmetic_mode::SUB:
-                    context.gpr[instr.reg1] = context.gpr[instr.reg2] - context.gpr[instr.reg3];
+                    context.gpr[reg_a] = context.gpr[reg_b] - context.gpr[reg_c];
                     break;
                 case arithmetic_mode::MUL:
-                    context.gpr[instr.reg1] = context.gpr[instr.reg2] * context.gpr[instr.reg3];
+                    context.gpr[reg_a] = context.gpr[reg_b] * context.gpr[reg_c];
                     break;
                 case arithmetic_mode::DIV:
-                    context.gpr[instr.reg1] = context.gpr[instr.reg2] / context.gpr[instr.reg3];
+                    context.gpr[reg_a] = context.gpr[reg_b] / context.gpr[reg_c];
                     break;
                 default: {
                     std::stringstream ss;
-                    ss << "Invalid mode for arithmetic instruction: " << instr.mode;
+                    ss << "Invalid mode for arithmetic instruction: " << mode;
                     throw std::logic_error(ss.str());
                 }
                 }
@@ -145,22 +152,22 @@ namespace m_emu {
             }
             case LOGIC: {
                 using logic_mode = common::instruction_t::logic_mode;
-                switch (static_cast<logic_mode>(instr.mode)) {
+                switch (static_cast<logic_mode>(mode)) {
                 case logic_mode::NOT:
-                    context.gpr[instr.reg1] = ~context.gpr[instr.reg2];
+                    context.gpr[reg_a] = ~context.gpr[reg_b];
                     break;
                 case logic_mode::AND:
-                    context.gpr[instr.reg1] = context.gpr[instr.reg2] & context.gpr[instr.reg3];
+                    context.gpr[reg_a] = context.gpr[reg_b] & context.gpr[reg_c];
                     break;
                 case logic_mode::OR:
-                    context.gpr[instr.reg1] = context.gpr[instr.reg2] | context.gpr[instr.reg3];
+                    context.gpr[reg_a] = context.gpr[reg_b] | context.gpr[reg_c];
                     break;
                 case logic_mode::XOR:
-                    context.gpr[instr.reg1] = context.gpr[instr.reg2] ^ context.gpr[instr.reg3];
+                    context.gpr[reg_a] = context.gpr[reg_b] ^ context.gpr[reg_c];
                     break;
                 default: {
                     std::stringstream ss;
-                    ss << "Invalid mode for logic instruction: " << instr.mode;
+                    ss << "Invalid mode for logic instruction: " << mode;
                     throw std::logic_error(ss.str());
                 }
                 }
@@ -168,17 +175,16 @@ namespace m_emu {
             }
             case SHIFT: {
                 using shift_mode = common::instruction_t::shift_mode;
-                switch (static_cast<shift_mode>(instr.mode)) {
-                    break;
+                switch (static_cast<shift_mode>(mode)) {
                 case shift_mode::LEFT:
-                    context.gpr[instr.reg1] = context.gpr[instr.reg2] << context.gpr[instr.reg3];
+                    context.gpr[reg_a] = context.gpr[reg_b] << context.gpr[reg_c];
                     break;
                 case shift_mode::RIGHT:
-                    context.gpr[instr.reg1] = context.gpr[instr.reg2] >> context.gpr[instr.reg3];
+                    context.gpr[reg_a] = context.gpr[reg_b] >> context.gpr[reg_c];
                     break;
                 default: {
                     std::stringstream ss;
-                    ss << "Invalid mode for shift instruction: " << instr.mode;
+                    ss << "Invalid mode for shift instruction: " << mode;
                     throw std::logic_error(ss.str());
                 }
                 }
@@ -186,26 +192,26 @@ namespace m_emu {
             }
             case STORE: {
                 using store_mode = common::instruction_t::store_mode;
-                switch (static_cast<store_mode>(instr.mode)) {
+                switch (static_cast<store_mode>(mode)) {
                 case store_mode::REG_DISP_IND: {
-                    context.write_to(context.gpr[instr.reg1] + context.gpr[instr.reg2] + instr.disp,
-                                     context.gpr[instr.reg3]);
+                    context.write_to(context.gpr[reg_a] + context.gpr[reg_b] + disp,
+                                     context.gpr[reg_c]);
                     break;
                 }
                 case store_mode::MEM_REG_DISP_IND: {
-                    const auto location_address = context.gpr[instr.reg1] + context.gpr[instr.reg2] + instr.disp;
+                    const auto location_address = context.gpr[reg_a] + context.gpr[reg_b] + disp;
                     const auto store_address = context.read_word(location_address);
-                    context.write_to(store_address, context.gpr[instr.reg3]);
+                    context.write_to(store_address, context.gpr[reg_c]);
                     break;
                 }
                 case store_mode::REG_IND_PREINC: {
-                    context.gpr[instr.reg1] = context.gpr[instr.reg1] + instr.disp;
-                    context.write_to(context.gpr[instr.reg1], context.gpr[instr.reg3]);
+                    context.gpr[reg_a] = context.gpr[reg_a] + disp;
+                    context.write_to(context.gpr[reg_a], context.gpr[reg_c]);
                     break;
                 }
                 default: {
                     std::stringstream ss;
-                    ss << "Invalid mode for store instruction: " << instr.mode;
+                    ss << "Invalid mode for store instruction: " << mode;
                     throw std::logic_error(ss.str());
                 }
                 }
@@ -213,40 +219,40 @@ namespace m_emu {
             }
             case LOAD: {
                 using load_mode = common::instruction_t::load_mode;
-                switch (static_cast<load_mode>(instr.mode)) {
+                switch (static_cast<load_mode>(mode)) {
                 case load_mode::READ_CSR:
-                    context.gpr[instr.reg1] = context.csr[instr.reg2];
+                    context.gpr[reg_a] = context.csr[reg_b];
                     break;
                 case load_mode::REG_MOVE_INC:
-                    context.gpr[instr.reg1] = context.gpr[instr.reg2] + instr.disp;
+                    context.gpr[reg_a] = context.gpr[reg_b] + disp;
                     break;
                 case load_mode::REG_DISP_IND:
-                    context.gpr[instr.reg1] = context.read_word(
-                        context.gpr[instr.reg2] + context.gpr[instr.reg3] + instr.disp
+                    context.gpr[reg_a] = context.read_word(
+                        context.gpr[reg_b] + context.gpr[reg_c] + disp
                     );
                     break;
                 case load_mode::REG_DISP_IND_POSTINC:
-                    context.gpr[instr.reg1] = context.read_word(context.gpr[instr.reg2]);
-                    context.gpr[instr.reg2] = context.gpr[instr.reg2] + instr.disp;
+                    context.gpr[reg_a] = context.read_word(context.gpr[reg_b]);
+                    context.gpr[reg_b] = context.gpr[reg_b] + disp;
                     break;
                 case load_mode::WRITE_CSR:
-                    context.csr[instr.reg1] = context.gpr[instr.reg2];
+                    context.csr[reg_a] = context.gpr[reg_b];
                     break;
                 case load_mode::WRITE_CSR_MOVE_OR:
-                    context.csr[instr.reg1] = context.csr[instr.reg2] | instr.disp;
+                    context.csr[reg_a] = context.csr[reg_b] | disp;
                     break;
                 case load_mode::WRITE_CSR_REG_DISP_IND:
-                    context.csr[instr.reg1] = context.read_word(
-                        context.gpr[instr.reg2] + context.gpr[instr.reg3] + instr.disp
+                    context.csr[reg_a] = context.read_word(
+                        context.gpr[reg_b] + context.gpr[reg_c] + disp
                     );
                     break;
                 case load_mode::WRITE_CSR_REG_DISP_IND_POSTINC:
-                    context.csr[instr.reg1] = context.read_word(context.gpr[instr.reg2]);
-                    context.gpr[instr.reg2] = context.gpr[instr.reg2] + instr.disp;
+                    context.csr[reg_a] = context.read_word(context.gpr[reg_b]);
+                    context.gpr[reg_b] = context.gpr[reg_b] + disp;
                     break;
                 default: {
                     std::stringstream ss;
-                    ss << "Invalid mode for load instruction: " << instr.mode;
+                    ss << "Invalid mode for load instruction: " << mode;
                     throw std::logic_error(ss.str());
                 }
                 }
@@ -254,7 +260,7 @@ namespace m_emu {
             }
             default: {
                 std::stringstream ss;
-                ss << "Unknow opcode: " << instr.code;
+                ss << "Unknow opcode: " << code;
                 ss << "; pc = " << std::hex << context.pc - 4;
                 throw std::logic_error(ss.str());
             }
--- src/emulator/main.cpp
+++ src/emulator/main.cpp
@@ -15,51 +15,28 @@ namespace m_emu {
 }
 
 int main(const int argc, char **argv) {
-    // try {
-    if (argc < 2) {
-        std::cerr << "Hex file argument missing.\n";
-        return MISSING_ARGS;
-    }
+    try {
+        if (argc < 2) {
+            std::cerr << "Hex file argument missing.\n";
+            return MISSING_ARGS;
+        }
 
-    common::symbol::exec_file_t exec_file;
-    std::ifstream ifs(argv[1], std::ios::binary);
-    common::util::serde::deserialize(ifs, exec_file);
-    ifs.close();
+        common::symbol::exec_file_t exec_file;
+        std::ifstream ifs(argv[1], std::ios::binary);
+        common::util::serde::deserialize(ifs, exec_file);
+        ifs.close();
 
-    std::cout << exec_file << '\n';
+        std::cout << exec_file << '\n';
 
-    m_emu::context_t context(exec_file.data);
-    emulate(context);
+        m_emu::context_t context(exec_file.data);
+        emulate(context);
 
-    std::cout << std::hex << std::setfill('0');
-    std::cout << "value 1: " << std::setw(2) << +context.memory[0] << std::setw(2) << +context.memory[1] << std::setw(2)
-            << +context.memory[2] << std::setw(2) << +context.memory[3] << '\n';
-    std::cout << "value 2: " << std::setw(2) << +context.memory[4] << std::setw(2) << +context.memory[5] << std::setw(2)
-            << +context.memory[6] << std::setw(2) << +context.memory[7] << '\n';
-    std::cout << "value 3: " << std::setw(2) << +context.memory[8] << std::setw(2) << +context.memory[9] << std::setw(2)
-            << +context.memory[10] << std::setw(2) << +context.memory[11] << '\n';
-    std::cout << "value 4: " << std::setw(2) << +context.memory[12] << std::setw(2) << +context.memory[13] <<
-            std::setw(2) << +context.memory[14] << std::setw(2) << +context.memory[15] << '\n';
-    std::cout << "value 5: " << std::setw(2) << +context.memory[16] << std::setw(2) << +context.memory[17] <<
-            std::setw(2) << +context.memory[18] << std::setw(2) << +context.memory[19] << '\n';
-    std::cout << "value 6: " << std::setw(2) << +context.memory[20] << std::setw(2) << +context.memory[21] <<
-            std::setw(2) << +context.memory[22] << std::setw(2) << +context.memory[23] << '\n';
-    std::cout << "value 7: " << std::setw(2) << +context.memory[24] << std::setw(2) << +context.memory[25] <<
-            std::setw(2) << +context.memory[26] << std::setw(2) << +context.memory[27] << '\n';
-    std::cout << std::dec << std::setfill(' ');
-
-    std::cout << std::hex;
-    std::cout << "status: " << context.status << '\n';
-    std::cout << "handler: " << context.handler << '\n';
-    std::cout << "cause: " << context.cause << '\n';
-    std::cout << std::dec;
-
-    std::cout << "Emulated processor executed halt instruction" << std::endl;
-    std::cout << "Emulated processor state:" << std::endl;
-    std::cout << context << '\n';
-    /*} catch (std::exception &err) {
+        std::cout << "Emulated processor executed halt instruction" << std::endl;
+        std::cout << "Emulated processor state:" << std::endl;
+        std::cout << context << '\n';
+    } catch (std::exception &err) {
         std::cerr << err.what() << '\n';
 
         return RUNTIME_ERROR;
-    }*/
+    }
 }
